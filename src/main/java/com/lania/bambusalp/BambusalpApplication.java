0ed583d97d89465faaef984b59c7abaee36ca299
Enrique Dorantes <enriquedorantes@MacBook-Pro-de-Enrique.local>
2019-05-22 21:00:22 -0500

    first commit

---
+++ src/main/java/com/lania/bambusalp/BambusalpApplication.java
@@ -0,0 +1,13 @@
+package com.lania.bambusalp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambusalpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambusalpApplication.class, args);
+	}
+
+}
